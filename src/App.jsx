68b2a449a757a5adb99c9220210a0719bc61d005
Qnosin <putaj.jakub@gmail.com>
2024-01-11 18:31:58 +0100

    feat: added react-router-dom lib and make basic routing system

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,47 @@
 import { useState } from "react";
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+import { Portfolio } from "./pages/Portfolio";
+import { Contact } from "./pages/Contact";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  Link,
+  Outlet,
+  RouterProvider,
+} from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0);
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Root />}>
+        <Route index element={<Home />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+      </Route>
+    )
+  );
+
+  const Root = () => {
+    return (
+      <>
+        <div>
+          <Link to="/">Home</Link>
+          <Link to="/data">Data</Link>
+        </div>
+        <div>
+          <Outlet />
+        </div>
+      </>
+    );
+  };
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline"> Hello im Jakub</h1>
+      <RouterProvider router={router} />
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,14 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      bgcolor: "#E5E7E9",
+      secondary: "#576061",
+    },
+    fontFamily: {
+      special: ["Jacques Francois Shadow", "serif"],
+      regular: ["Anonymous Pro", "monospace"],
+    },
   },
   plugins: [],
 };

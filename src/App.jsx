ba24421382f9bcc49d478ae2b608000e50759959
Qnosin <putaj.jakub@gmail.com>
2024-01-11 19:14:46 +0100

    feat: aded a navigation menu and mobile hamburger menu

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Portfolio from "./pages/Portfolio";
 import Contact from "./pages/Contact";
+import { Turn as Hamburger } from "hamburger-react";
 import {
   createBrowserRouter,
   createRoutesFromElements,
@@ -13,7 +14,6 @@ import {
 } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0);
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<Root />}>
@@ -27,21 +27,36 @@ function App() {
 
   return (
     <>
-      <section>
-        <RouterProvider router={router} />
-      </section>
+      <main className="relative flex justify-center align-center ">
+        <div className="container p-4  ">
+          <div className="svg-image">
+            <img
+              src="../public/line.png"
+              className="absolute left-0 top-0"
+              alt="svg-line"
+            />
+          </div>
+          <RouterProvider router={router} />
+        </div>
+      </main>
     </>
   );
 }
 const Root = () => {
+  const [isOpen, setOpen] = useState(false);
   return (
     <>
-      <div className="nav">
-        <Link to="/">Home</Link>
-        <Link to="/portfolio">Portfolio</Link>
-        <Link to="/contact">Contact</Link>
-        <Link to="/about">About</Link>
+      <div className="hamburger flex justify-end z-10 relative">
+        <Hamburger toggled={isOpen} toggle={setOpen} />
       </div>
+      {isOpen && (
+        <div className="nav absolute w-full top-0 right-0 h-screen flex flex-col justify-center items-center z-0 bg-[#D9D9D9] ">
+          <Link to="/">Home</Link>
+          <Link to="/portfolio">Portfolio</Link>
+          <Link to="/contact">Contact</Link>
+          <Link to="/about">About</Link>
+        </div>
+      )}
       <Outlet />
     </>
   );
